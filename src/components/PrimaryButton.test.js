5886ad4749cc0c0b913932ec77e37af68119a76c
Lovro Kalan <lovro.kalan1@gmail.com>
2021-06-29 15:34:26 +0200

    Fix primary button import in test

--- src/components/PrimaryButton.test.js
+++ src/components/PrimaryButton.test.js
@@ -2,12 +2,15 @@ import "@testing-library/jest-dom";
 
 import * as React from "react";
 import { render, fireEvent, screen } from "@testing-library/react";
-import Button from "./Button";
+import PrimaryButton from "./PrimaryButton";
 
 test("Clicking a button should trigger the function passed in onClick prop", () => {
   let buttonWasClicked = false;
   render(
-    <Button title="Button title" onClick={() => (buttonWasClicked = true)} />
+    <PrimaryButton
+      title="Button title"
+      onClick={() => (buttonWasClicked = true)}
+    />
   );
   fireEvent.click(screen.queryByText("Button title"));
   expect(buttonWasClicked).toBeTruthy();
@@ -16,7 +19,7 @@ test("Clicking a button should trigger the function passed in onClick prop", () 
 test("Clicking a disabled button shouldn't trigger the function passed in onClick prop", () => {
   let buttonWasClicked = false;
   render(
-    <Button
+    <PrimaryButton
       title="Button title"
       disabled={true}
       onClick={() => (buttonWasClicked = true)}
